edb35cc81762a5686633e73325d1dd0bba71914a
Cezary Baginski <cezary@chronomantic.net>
2015-04-07 22:47:02 +0200

    avoid sleeps in specs + reduce flakyness

--- spec/celluloid/pool/supervision_group_spec.rb
+++ spec/celluloid/pool/supervision_group_spec.rb
@@ -1,28 +1,46 @@
 RSpec.describe Celluloid::SupervisionGroup, actor_system: :global do
 
-  context "pool" do
-    before :all do
-      class MyPoolActor
-        include Celluloid
+  class SupervisionGroupHelper
+    QUEUE = Queue.new
+    SIZE = ::POOL_SIZE
+  end
 
-        attr_reader :args
-        def initialize *args
-          @args = *args
-        end
-        def running?; :yep; end
-      end
-      class MyPoolGroup < Celluloid::SupervisionGroup
-        pool MyPoolActor, :as => :example_pool, :args => 'foo', :size => ::POOL_SIZE
-      end
+  class MyPoolActor
+    include Celluloid
+
+    attr_reader :args
+    def initialize *args
+      @args = *args
+      ready
+    end
+
+    def running?
+      :yep
+    end
+
+    def ready
+      SupervisionGroupHelper::QUEUE << :done
+    end
+  end
+
+  context "when supervising a pool" do
+    let(:size) { SupervisionGroupHelper::SIZE }
+
+    before do
+      subject
+      size.times { SupervisionGroupHelper::QUEUE.pop }
+    end
+
+    subject do
+      Class.new(Celluloid::SupervisionGroup) do
+        pool MyPoolActor, :as => :example_pool, :args => 'foo', :size => SupervisionGroupHelper::SIZE
+      end.run!
     end
 
     it "runs applications and passes pool options and actor args" do
-      MyPoolGroup.run!
-      sleep 0.001 # startup time hax
-
       expect(Celluloid::Actor[:example_pool]).to be_running
       expect(Celluloid::Actor[:example_pool].args).to eq ['foo']
-      expect(Celluloid::Actor[:example_pool].size).to eq ::POOL_SIZE
+      expect(Celluloid::Actor[:example_pool].size).to be size
     end
   end
 end
